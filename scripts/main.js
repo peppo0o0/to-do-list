3f2ecbaf12995251fcd6974976a03eb47404c8cc
Daniel García <144857230+peppo0o0@users.noreply.github.com>
2023-09-17 13:50:27 -0400

    Check

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".bar input[type=text]");
   const taskList = document.querySelector(".list-group");
 
+  // Seleccionar
+  var list = document.querySelector('ul');
+  list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+    }
+  }, false);
+
   // Boton Add
   addButton.onclick = () => {
-      const taskText = input.value;
-      if (taskText !== "") {
-          const newItem = document.createElement("li");
-          newItem.className = "list-group-item";
-          newItem.textContent = taskText;
-          taskList.appendChild(newItem);
-          input.value = "";
-      }
+    const taskText = input.value;
+    if (taskText !== "") {
+        const newItem = document.createElement("li");
+        newItem.className = "list-group-item";
+        newItem.textContent = taskText;
+        taskList.appendChild(newItem);
+        input.value = "";
+    }
   };
 
+  // Boton Delete
+  
 });
