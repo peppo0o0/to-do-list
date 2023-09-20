10a6e1cfc313f1f9051527afe82c410029d2333a
Alfredo <alfredogmreal45@gmail.com>
2023-09-20 17:44:30 -0400

    Add delete solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addButton = document.getElementById("addButton");
-  const deleteButton = document.getElementById("deleteButton");
   const backButton = document.getElementById("backButton");
   const input = document.querySelector(".bar input[type=text]");
   const taskList = document.querySelector(".list-group");
 
-  let tasks = []
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || []
   let searchedTasks = []
+
+  const generateDeleteButton = (task) => {
+    const button = document.createElement('button')
+    button.innerText = 'Eliminar'
+    button.addEventListener('click', function () {
+      deleteTask(task)
+    })
+    button.style = 'float: right'
+    return button
+  }
+
+  const generateLi = (task) => {
+    const li = document.createElement('li')
+    li.innerText = task.value
+    li.className = 'list-group-item'
+    li.appendChild(generateDeleteButton(task))
+    return li
+   }
+
+  const printTasks = (tasks) => {
+    taskList.innerHTML = ''
+    tasks.forEach((task) => taskList.appendChild(generateLi(task)))
+  }
+
+  printTasks(tasks)
+ 
   const cleanInput = () => {
     input.value = ''
   }
 
   const addTask = (task) => {
     if(task) {
-      tasks.push(task.trim())
+      tasks.push({ value: task.trim(), id: crypto.randomUUID() })
+      localStorage.setItem('tasks', JSON.stringify(tasks))
       cleanInput()
     }
   }
 
   const deleteTask = (taskToDelete) => {
-    tasks = tasks.filter(task => taskToDelete !== task)
+    tasks = tasks.filter(task => taskToDelete.id !== task.id)
+    printTasks(tasks)
   }
-
-  const printTasks = (tasks) => {
-    const content = `${tasks.map((task) => `<li class="list-group-item">
-      ${task} <button value="${task}" style="float: right;">Eliminar</button>
-    </li>`)}`
-    taskList.innerHTML = content
-  }
+  
+  
 
   const searchTask = (taskToSearch) => {
-    searchedTasks = tasks.filter(task => taskToSearch.trim().toLowerCase() === task.toLowerCase())
+    searchedTasks = tasks.filter(task => taskToSearch.trim().toLowerCase() === task.value.toLowerCase())
     cleanInput()
   }
   
-  deleteButton.onclick = (task) => {
-    deleteTask(task)
-    printTasks(tasks)
-  }
   // Add Button
   addButton.onclick = () => {
     addTask(input.value)
@@ -52,58 +70,4 @@ document.addEventListener("DOMContentLoaded", () => {
   backButton.onclick = () => {
     printTasks(tasks)
   }
-  
-
-  // // Check
-  // var list = document.querySelector('ul');
-  // list.addEventListener('click', function(ev) {
-  //   if (ev.target.tagName === 'LI') {
-  //     ev.target.classList.toggle('checked');
-  //   }
-  // }, false);
-
-  // // Add Button
-  // addButton.onclick = () => {
-  //   const taskText = input.value;
-  //   if (taskText) {
-  //       const newItem = document.createElement("li");
-  //       newItem.className = "list-group-item";
-  //       newItem.textContent = taskText;
-  //       taskList.appendChild(newItem);
-  //       input.value = "";
-  //   }
-  // };
-  
-  // // Delete Button
-  // deleteButton.onclick = () => {
-  //   const selectedItems = document.querySelectorAll('.list-group-item.checked');
-  //   selectedItems.forEach(function (item) {
-  //     item.remove();
-  //   });
-  // };
-
-  // // Search Button
-  // searchButton.onclick = () => {
-  //   const searchInput = input.value.trim();
-  //   const listItems = document.querySelectorAll('.list-group-item');
-  //   if(searchInput) {
-  //     listItems.forEach(function (item) {
-  //       const itemValue = item.textContent;
-  //       if (itemValue.toLowerCase() === searchInput.toLowerCase()) {
-  //         item.style.display = "block";
-  //       } else {
-  //         item.style.display= "none";
-  //       }
-  //     });
-  //   }
-  // };
-
-  // Back Button 
-  // backButton.onclick = () => {
-  //   const listItems = document.querySelectorAll('.list-group-item');
-  //   input.value = ""
-  //   listItems.forEach(function (item){
-  //     item.style.display = "block";
-  //   }
-  // )};
 });
