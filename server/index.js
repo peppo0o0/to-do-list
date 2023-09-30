1ba446126004a88c8eee4ac39cc6efc90bd8816e
peppo0o0 <peppo0o01337@gmail.com>
2023-09-30 11:22:58 -0400

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+// get the client
+const express = require('express');
+const app = express();
+const port = 3000;
 const mysql = require('mysql2');
+app.set("port", process.env.PORT || 3000);
+
+// connection to the database
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -11,20 +14,51 @@ const connection = mysql.createConnection({
   port: 3306
 });
 
-app.set("port", process.env.PORT || 3000);
+// middlewares
+app.use(express.json());
 
+// GET, POST, DELETE & PUT
 app.get('/', (req, res) => {
 
-connection.connect();
+  connection.connect();
  
-connection.query('SELECT * FROM tasks', function (error, results, fields) {
-  if (error) throw error;
-  res.json(results)
+  connection.execute('SELECT * FROM tasks', function (error, results, fields) {
+    if (error) throw error;
+    res.json(results)
+  });
 });
 
-connection.end();
-})
+app.post('/add', (req, res) => {
+  
+  connection.connect();
 
+  connection.query('INSERT INTO tasks set ?', [req.body],function (error, results, fields) {
+    if (error) throw error;
+    res.redirect("/");
+  });
+});
+
+app.delete('/:id', (req, res) => {
+  
+  connection.connect();
+
+  connection.execute('DELETE FROM tasks WHERE id = ?', [req.params.id],function (error, results, fields) {
+    if (error) throw error;
+    res.redirect("/");
+  });
+});
+
+app.put('/:id', (req, res) => {
+  
+  connection.connect();
+
+  connection.query('UPDATE tasks set ? WHERE id = ?', [req.body, req.params.id],function (error, results, fields) {
+    if (error) throw error;
+    res.redirect("/");
+  });
+});
+
+// server running
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
