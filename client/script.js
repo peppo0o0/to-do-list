326e10bfc610834356884434e6c2ef76f014837c
peppo0o0 <peppo0o01337@gmail.com>
2023-10-09 14:44:57 -0400

    update

--- client/script.js
+++ client/script.js
@@ -1,13 +1,23 @@
 const listTask = document.querySelector(".list-group");
 const btnCreate = document.getElementById("btn-create");
+const btnSearch = document.getElementById("btn-search");
+const btnBack = document.getElementById("btn-back");
+const input = document.querySelector("input[type=text]");
 const apiUrl = 'http://localhost:3000/api/task'
+
+// Task checker 
+listTask.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 
 // Creating task with their delete button 
 async function createTaskLi(post){
   const li = document.createElement('li')
   li.innerText = post.task
   const button = document.createElement('button')
-  button.innerText = 'DELETE'
+  button.innerText = 'X'
   button.className = 'delete-button'
 
   // Delete Button
@@ -31,7 +41,7 @@ async function getTasks() {
 };
 getTasks();
 
-// FETCH API (POST)
+// Fetch API (POST)
 async function createTask(newTask) {
   if(newTask) {
     await fetch(apiUrl,{
@@ -57,3 +67,36 @@ async function delTasks(id) {
     method: 'DELETE'
   });
 };
+
+// Search Function
+async function search(){
+  const searchInput = input.value;
+  const listItems = document.querySelectorAll('ul li');
+  listItems.forEach(function (item) {
+    const itemValue = item.textContent;
+    if (itemValue.toLowerCase().includes(searchInput.toLowerCase())) {
+      item.style.display = "block";
+    } else {
+      item.style.display= "none";
+    }
+  });
+}
+
+// Search Button
+btnSearch.addEventListener('click', async () => {
+  await search()
+});
+
+// Back Function
+async function back(){
+  const listItems = document.querySelectorAll('ul li');
+  listItems.forEach(function (item){
+    item.style.display = "block";
+    input.value = "";
+  });
+};
+
+// Back Button
+btnBack.addEventListener('click', async () => {
+  await back();
+});
